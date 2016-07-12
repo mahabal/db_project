20a23f7cebbf1d1386881a0db6cf4080da071300
Matt <matthew@mahabal.org>
2016-07-11 22:46:57 -0400

    Handle login attempts and session generation

--- src/main/java/org/mahabal/project/servlet/LoginServlet.java
+++ src/main/java/org/mahabal/project/servlet/LoginServlet.java
@@ -1,5 +1,6 @@
 package org.mahabal.project.servlet;
 
+import com.google.common.hash.Hashing;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
 import org.skife.jdbi.v2.DBI;
@@ -9,6 +10,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.List;
 import java.util.Map;
 
@@ -34,10 +36,12 @@ public class LoginServlet extends ProjectServlet {
             return;
         }
 
+        int id;
+        String hash, salt;
         try (Handle h = dbi.open()) {
 
             List<Map<String, Object>> user = h.select("select uid, password, salt from student where username = ?",
-                    username);
+                    username.toLowerCase());
 
             if (user.size() == 0) {
                 // there is no student with the provided username
@@ -46,11 +50,61 @@ public class LoginServlet extends ProjectServlet {
                 o.add("status", new JsonPrimitive("error"));
                 o.add("desc", new JsonPrimitive("invalid username or password"));
                 resp.getWriter().println(o);
+                return;
+            } else {
+                id = (Integer) user.get(0).get("uid");
+                hash = (String) user.get(0).get("password");
+                salt = (String) user.get(0).get("salt");
             }
 
         }
 
-        resp.setStatus(HttpServletResponse.SC_OK);
+        // create a hash using the provided password and the recently retrieved salt
+        String test = Hashing.sha512().hashString(salt + md5pass, Charset.forName("UTF-8")).toString()
+                .substring(0, 36);
+
+        // see if the test hash is the same as the hash received from the database
+        if (test.equals(hash)) {
+
+            // stores the token that will be used for this user
+            String token;
+
+            try (Handle h = dbi.open()) {
+
+                // check and see if a token already exists for this userId + IP
+                List<Map<String, Object>> tokens = h.select("select token from session where uid = ? and " +
+                        "ip = INET6_ATON(?)", id, ip);
+
+                if (tokens.size() == 0) {
+                    // no token exists, so create one (36 chars) and save it into the database
+                    token = Hashing.sha512().hashString(id + "-" + ip + "-" + Math.random(),
+                            Charset.forName("UTF-8")).toString().substring(0, 36);
+                    h.insert("insert into session (uid, ip, token) values (?,INET6_ATON(?),?)",
+                            id, ip, token);
+                } else {
+                    token = (String) tokens.get(0).get("token");
+                }
+            }
+
+            final JsonObject o = new JsonObject();
+            o.add("uid", new JsonPrimitive(id));
+            o.add("token", new JsonPrimitive(token));
+            resp.setStatus(HttpServletResponse.SC_OK);
+            resp.getWriter().println(o);
+
+        } else {
+
+            System.err.println("INVALID LOGIN!");
+            resp.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            final JsonObject o = new JsonObject();
+            o.add("status", new JsonPrimitive("error"));
+            o.add("desc", new JsonPrimitive("invalid username or password"));
+            resp.getWriter().println(o);
+            return;
+
+        }
+
+        resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
 
     }
 
