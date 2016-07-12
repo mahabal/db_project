924bc70c470b201ea5e502f4b7446546e6ea5616
Matt <matthew@mahabal.org>
2016-07-11 22:14:21 -0400

    Code cleanup and add whitespace for easier reading

--- src/main/java/org/mahabal/project/servlet/RegisterServlet.java
+++ src/main/java/org/mahabal/project/servlet/RegisterServlet.java
@@ -23,6 +23,10 @@ public class RegisterServlet extends ProjectServlet {
 
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        // get the IP address of the request
+        String ip = req.getHeader("X-Forwarded-For");
+        if (ip == null) ip = req.getRemoteAddr();
+
         // load the requested username, email, and password from the request
         String username = req.getParameter("u");
         String email = req.getParameter("e");
@@ -36,11 +40,14 @@ public class RegisterServlet extends ProjectServlet {
         }
 
         try (Handle h = dbi.open()) {
+
             // query the `student` table to search for the username
             List<Map<String, Object>> user = h.select("select uid from student where username = ?",
                     username.toLowerCase());
+
             // see how many users are in the result set
             if (user.size() == 0) {
+
                 // there are no users in the result set, that means we can create the user.
                 // generate a salt to use with hashing the password, then trim it to 12 characters
                 final String salt = Hashing.sha512().hashString(String.valueOf(new Random().nextDouble()),
@@ -48,25 +55,34 @@ public class RegisterServlet extends ProjectServlet {
                 // generate a newly salted hash from the md5 password, them trim it to 36 characters
                 final String pwHash = Hashing.sha512().hashString(salt + md5pass, Charset.forName("UTF-8"))
                         .toString().substring(0, 36);
+
                 // insert data into the student table
                 h.insert("insert into student (username, password, salt, email) values(?,?,?,?)",
                         username, pwHash, salt, email);
+
                 // create json object with return data
                 final JsonObject object = new JsonObject();
                 object.add("status", new JsonPrimitive("success"));
+
                 // set the response code and print out the object
                 resp.setStatus(HttpServletResponse.SC_OK);
                 resp.getWriter().println(object);
-                //TODO: Generate a new session into the session table.
+
             } else {
+
                 final JsonObject obj = new JsonObject();
                 obj.add("status", new JsonPrimitive("error"));
                 obj.add("description", new JsonPrimitive("user exists"));
+
                 resp.setStatus(HttpServletResponse.SC_CONFLICT);
                 resp.getWriter().println(obj);
+
             }
+
         }
+
         resp.setStatus(HttpServletResponse.SC_OK);
+
     }
 
 }
