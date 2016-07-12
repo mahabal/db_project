9e79358008526674b565e53befd5949c701da86f
Matt <matthew@mahabal.org>
2016-07-11 22:21:33 -0400

    Create LoginServlet

---
+++ src/main/java/org/mahabal/project/servlet/LoginServlet.java
@@ -0,0 +1,48 @@
+package org.mahabal.project.servlet;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import org.skife.jdbi.v2.DBI;
+import org.skife.jdbi.v2.Handle;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+public class LoginServlet extends ProjectServlet {
+
+    public LoginServlet(DBI dbi) {
+        super(dbi);
+    }
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        // get IP address
+        String ip = req.getHeader("X-Forwarded-For");
+        if (ip == null) ip = req.getRemoteAddr();
+
+        // load the requested username and password from the request
+        String username = req.getParameter("u");
+        String md5pass = req.getParameter("m");
+
+        // check and make sure the username and password are not null
+        if (username == null || md5pass == null) {
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        try (Handle h = dbi.open()) {
+
+            List<Map<String, Object>> user = h.select("select uid, password, salt from student where username = ?",
+                    username);
+
+        }
+
+        resp.setStatus(HttpServletResponse.SC_OK);
+
+    }
+
+}
