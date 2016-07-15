cbb35b9bdd54229b968ba4eac4f4ce545109d90f
Matt <matthew@mahabal.org>
2016-07-15 17:41:55 -0400

    Automatic code reformatting

--- src/main/resources/html/js/project.js
+++ src/main/resources/html/js/project.js
@@ -1,192 +1,191 @@
-(function(global) {
+(function (global) {
     'use strict';
 
-        var token;
-        var uid;
+    var token;
+    var uid;
 
-        var validateSession = function() {
+    var validateSession = function () {
 
-            // read the cookies into variables for easy access
-            token = Cookies.get("project_token");
-            uid = Cookies.get("project_uid");
+        // read the cookies into variables for easy access
+        token = Cookies.get("project_token");
+        uid = Cookies.get("project_uid");
 
-            // use ajax to connect to the login api and make sure the session is valid
-            $.ajax({
-                url: API_BASE_URL + "/login",
-                type: 'GET',
-                data: {
-                    'i': uid,
-                    's': token
-                },
-                success: function() {
-                    // session checks out ... do nothing, let the page load like normal
-                },
-                error: function(response) {
-                    // session is not valid ... purge everything and then load the login screen.
-                    logout();
+        // use ajax to connect to the login api and make sure the session is valid
+        $.ajax({
+            url: API_BASE_URL + "/login",
+            type: 'GET',
+            data: {
+                'i': uid,
+                's': token
+            },
+            success: function () {
+                // session checks out ... do nothing, let the page load like normal
+            },
+            error: function (response) {
+                // session is not valid ... purge everything and then load the login screen.
+                logout();
+            }
+        });
+
+        return false;
+    };
+
+    var initDashboard = function () {
+        // use ajax to connect to the login api and make sure the session is valid
+        $.ajax({
+            url: API_BASE_URL + "/dashboard",
+            type: 'GET',
+            data: {
+                'i': uid,
+                's': token
+            },
+            success: function (data) {
+                var json = JSON.parse(data);
+                console.log(json);
+
+                for (var key in json) {
+                    if (json.hasOwnProperty(key)) {
+                        // get the block
+                        var block = $('#' + key + "_block");
+                        if (json[key] < 0) block.addClass("hidden");
+                        else block.removeClass("hidden");
+                        // get the text
+                        var text = $('#' + key);
+                        text.text(json[key]);
+                    }
                 }
-            });
 
-            return false;
-        };
+            },
+            error: function (response) {
+                // session is not valid ... purge everything and then load the login screen.
+                logout();
+            }
+        });
+    };
 
-        var initDashboard = function() {
-            // use ajax to connect to the login api and make sure the session is valid
-            $.ajax({
-                url: API_BASE_URL + "/dashboard",
-                type: 'GET',
-                data: {
-                    'i': uid,
-                    's': token
-                },
-                success: function(data) {
-                    var json = JSON.parse(data);
-                    console.log(json);
-
-                    for (var key in json) {
-                        if (json.hasOwnProperty(key)) {
-                            // get the block
-                            var block = $('#' + key + "_block");
-                            if (json[key] < 0) block.addClass("hidden");
-                            else block.removeClass("hidden");
-                            // get the text
-                            var text = $('#' + key);
-                            text.text(json[key]);
+    var rso_success = function (data) {
+        var json = JSON.parse(data);
+        for (var type in json) {
+            if (json.hasOwnProperty(type)) {
+                var arr = json[type];
+                var block = $('#' + type + '_panel');
+                if (arr.length <= 0) block.addClass("hidden");
+                else block.removeClass("hidden");
+                for (var i = 0; i < arr.length; i++) {
+                    var o = arr[i];
+                    var row = $('<tr></tr>').attr("id", type + "_row_" + o["rid"]);
+                    for (var key in o) {
+                        if (o.hasOwnProperty(key)) {
+                            var element = $('<td></td>').attr("id", "td_" + i + "_" + key).text(o[key]);
+                            if (type === 'owned_rsos') {
+                                if (key === 'approved') {
+                                    if (o[key] === 0) {
+                                        element.text('').append('<span class=\"label label-warning\">Unapproved</span>');
+                                    } else if (o[key] === 1) {
+                                        element.text('').append('<span class=\"label label-success\">Approved</span>');
+                                    }
+                                }
+                            } else if (type === 'membership_rsos') {
+                                if (key === 'approved') {
+                                    element.text('');
+                                    if (o[key] === 0) {
+                                        row.addClass("warning")
+                                    }
+                                }
+                            }
+                            row.append(element);
                         }
                     }
+                    if (type === 'unapproved_rsos') {
+                        // create the button
 
-                },
-                error: function(response) {
-                    // session is not valid ... purge everything and then load the login screen.
-                    logout();
+                        row.append('<td><button class="btn btn-success btn-rounded btn-condensed btn-sm ' + (o['members'] < 5 ? 'disabled"' : '" onclick="Project.approve_row(\'' + type + '_row_' + o["rid"] + '\');"') + '><span class="fa fa-check"></span></button></td>');
+                        if (o['members'] >= 5) row.addClass("success");
+                    }
+                    var tbody = $('#' + type + '_tbody').append(row);
                 }
-            });
-        };
-
-        var rso_success = function(data) {
-                                var json = JSON.parse(data);
-                                for (var type in json) {
-                                    if (json.hasOwnProperty(type)) {
-                                        var arr = json[type];
-                                        var block = $('#' + type + '_panel');
-                                        if (arr.length <= 0) block.addClass("hidden");
-                                        else block.removeClass("hidden");
-                                        for (var i = 0; i < arr.length; i++) {
-                                            var o = arr[i];
-                                            var row = $('<tr></tr>').attr("id", type + "_row_" + o["rid"]);
-                                            for (var key in o) {
-                                                if (o.hasOwnProperty(key)) {
-                                                    var element = $('<td></td>').attr("id", "td_" + i+"_" + key).text(o[key]);
-                                                    if (type === 'owned_rsos') {
-                                                        if (key === 'approved') {
-                                                        if (o[key] === 0) {
-                                                        element.text('').append('<span class=\"label label-warning\">Unapproved</span>');
-                                                        } else if (o[key] === 1) {
-                                                           element.text('').append('<span class=\"label label-success\">Approved</span>');
-                                                           }
-                                                        }
-                                                    } else if (type === 'membership_rsos') {
-                                                        if (key === 'approved') {
-                                                                element.text('');
-                                                            if (o[key] === 0) {
-                                                                row.addClass("warning")
-                                                            }
-                                                        }
-                                                    }
-                                                    row.append(element);
-                                                }
-                                            }
-                                            if (type === 'unapproved_rsos') {
-                                                // create the button
-
-                                                row.append('<td><button class="btn btn-success btn-rounded btn-condensed btn-sm ' + (o['members'] < 5 ? 'disabled"' : '" onclick="Project.approve_row(\'' + type + '_row_' + o["rid"] + '\');"') + '><span class="fa fa-check"></span></button></td>');
-                                                if (o['members'] >= 5) row.addClass("success");
-                                                }
-                                                var tbody = $('#' + type + '_tbody').append(row);
-                                        }
-                                    }
-
-                                var table = $('#' + type + '_table');
-                                table.addClass('datatable');
-                                table.DataTable();
-                                }
-
-        };
-
-        var approve_row = function(row) {
-            var box = $("#mb-approve-row");
-            box.addClass("open");
-            box.find(".mb-control-yes").on("click",function(){
-                box.removeClass("open");
-                $('#' + row).hide("medium",function(){
-                    $(this).remove();
-                    $.ajax({
-                        url: API_BASE_URL + "/rsos",
-                        type: 'GET',
-                        data: {
-                            'i': uid,
-                            's': token,
-                            'a': 'approve',
-                            'n': row.split("_").pop()
-                        },
-                        success: location.reload(),
-                        error: function(data) {
-
-                        }
-                    });
-                });
-             });
-
-
-        };
-
-        var initRSOTables = function() {
-            // use ajax to connect to the login api and make sure the session is valid
-            $.ajax({
-                url: API_BASE_URL + "/rsos",
-                type: 'GET',
-                data: {
-                    'i': uid,
-                    's': token
-                },
-                success: rso_success,
-                error: function(response) {
-                    // session is not valid ... purge everything and then load the login screen.
-                    logout();
-                }
-            });
-        }
-
-
-
-        var logout = function() {
-            Cookies.remove("project_token");
-            Cookies.remove("project_uid");
-            Cookies.remove("project_username");
-            window.location = 'login.html';
-        };
-
-        var initLogoutButton = function() {
-            var logoutButton = $('#logout-link');
-            logoutButton.click(function() {
-                logout();
-            });
-        };
-
-        var init = function() {
-            validateSession();
-            initLogoutButton();
-            var page = location.pathname.split("/").pop();
-            if (page === 'index2.html') {
-                initDashboard();
-            } else if (page === 'rsos.html') {
-                initRSOTables();
             }
-        };
 
-        global.Project = {
-            init: init,
-            approve_row: approve_row
+            var table = $('#' + type + '_table');
+            table.addClass('datatable');
+            table.DataTable();
         }
+
+    };
+
+    var approve_row = function (row) {
+        var box = $("#mb-approve-row");
+        box.addClass("open");
+        box.find(".mb-control-yes").on("click", function () {
+            box.removeClass("open");
+            $('#' + row).hide("medium", function () {
+                $(this).remove();
+                $.ajax({
+                    url: API_BASE_URL + "/rsos",
+                    type: 'GET',
+                    data: {
+                        'i': uid,
+                        's': token,
+                        'a': 'approve',
+                        'n': row.split("_").pop()
+                    },
+                    success: location.reload(),
+                    error: function (data) {
+
+                    }
+                });
+            });
+        });
+
+
+    };
+
+    var initRSOTables = function () {
+        // use ajax to connect to the login api and make sure the session is valid
+        $.ajax({
+            url: API_BASE_URL + "/rsos",
+            type: 'GET',
+            data: {
+                'i': uid,
+                's': token
+            },
+            success: rso_success,
+            error: function (response) {
+                // session is not valid ... purge everything and then load the login screen.
+                logout();
+            }
+        });
+    }
+
+
+    var logout = function () {
+        Cookies.remove("project_token");
+        Cookies.remove("project_uid");
+        Cookies.remove("project_username");
+        window.location = 'login.html';
+    };
+
+    var initLogoutButton = function () {
+        var logoutButton = $('#logout-link');
+        logoutButton.click(function () {
+            logout();
+        });
+    };
+
+    var init = function () {
+        validateSession();
+        initLogoutButton();
+        var page = location.pathname.split("/").pop();
+        if (page === 'index2.html') {
+            initDashboard();
+        } else if (page === 'rsos.html') {
+            initRSOTables();
+        }
+    };
+
+    global.Project = {
+        init: init,
+        approve_row: approve_row
+    }
 
 })(this);
--- src/main/resources/html/js/register.js
+++ src/main/resources/html/js/register.js
@@ -1,52 +1,52 @@
-(function(global) {
+(function (global) {
     'use strict';
     var emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/i;
-    var validateRegistration = function() {
+    var validateRegistration = function () {
         var email = $('email').find('#register').val();
     };
-        var validation = function(str) {
-            var el = $('#error-banner');
-            el.removeClass("hidden");
-            var el2 = $('#error-msg')
-            el2.text(str);
-        };
-        var removeValidation = function() {
-            var el = $('#error-banner');
-            el.addClass("hidden");
-            var el2 = $('#error-msg')
-            el2.text("");
-        };
-        var login = function(username, password) {
-            if (typeof global.sha512 === 'undefined') {
-                console.error("Missing the sha2 include");
-                return false;
+    var validation = function (str) {
+        var el = $('#error-banner');
+        el.removeClass("hidden");
+        var el2 = $('#error-msg')
+        el2.text(str);
+    };
+    var removeValidation = function () {
+        var el = $('#error-banner');
+        el.addClass("hidden");
+        var el2 = $('#error-msg')
+        el2.text("");
+    };
+    var login = function (username, password) {
+        if (typeof global.sha512 === 'undefined') {
+            console.error("Missing the sha2 include");
+            return false;
+        }
+
+        var passwordSha512 = sha512(password);
+
+        $.ajax({
+            url: API_BASE_URL + '/login',
+            type: 'GET',
+            data: {
+                'u': username,
+                'm': passwordSha512
+            },
+            success: function (data) {
+                var json = JSON.parse(data);
+                Cookies.set("project_uid", json['uid'], {expires: 1});
+                Cookies.set("project_username", json['name'], {expires: 1});
+                Cookies.set("project_token", json['token'], {expires: 1});
+                console.log(Cookies.get("token"));
+                location = 'index2.html';
+            },
+            error: function (data) {
+                console.error("SOMETHING BROKE!");
             }
 
-            var passwordSha512 = sha512(password);
-
-            $.ajax({
-                url: API_BASE_URL + '/login',
-                type: 'GET',
-                data: {
-                        'u': username,
-                        'm': passwordSha512
-                    },
-                    success: function(data) {
-                        var json = JSON.parse(data);
-                        Cookies.set("project_uid", json['uid'], { expires: 1} );
-                        Cookies.set("project_username", json['name'], { expires: 1 });
-                        Cookies.set("project_token", json['token'], { expires: 1 });
-                        console.log(Cookies.get("token"));
-                        location = 'index2.html';
-                    },
-                    error: function(data) {
-                        console.error("SOMETHING BROKE!");
-                    }
-
-            });
-            return true;
-        };
-    var registerAccount = function(username, password, email) {
+        });
+        return true;
+    };
+    var registerAccount = function (username, password, email) {
         if (typeof global.sha512 === 'undefined') {
             console.error("Missing the sha2 include");
             return false;
@@ -61,37 +61,37 @@
                 'e': email,
                 'm': passwordSha512
             },
-            success: function() {
+            success: function () {
                 login(username, password);
             },
-            error: function(response) {
+            error: function (response) {
                 var data = JSON.parse(response.responseText);
                 validation(data['error']);
             }
         });
         return true;
     }
-    var initRegisterButton = function() {
+    var initRegisterButton = function () {
         var registerButton = $('#sign-up-button');
-        registerButton.click(function() {
-                removeValidation();
-                var username = $('#username').val();
-                var password = $('#password').val();
-                var email = $('#email').val();
-                registerAccount(username, password, email);
+        registerButton.click(function () {
+            removeValidation();
+            var username = $('#username').val();
+            var password = $('#password').val();
+            var email = $('#email').val();
+            registerAccount(username, password, email);
         });
     };
-    var initLoginButton = function() {
+    var initLoginButton = function () {
         var loginButton = $('#log-in-button');
 
-        loginButton.click(function() {
-                removeValidation();
-                var username = $('#username').val();
-                var password = $('#password').val();
-                login(username, password);
+        loginButton.click(function () {
+            removeValidation();
+            var username = $('#username').val();
+            var password = $('#password').val();
+            login(username, password);
         });
     };
-    var init = function() {
+    var init = function () {
         initRegisterButton();
         initLoginButton();
     };
