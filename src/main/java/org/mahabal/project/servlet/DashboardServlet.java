3bf00a904018aa2ed50d2ccad0b27ed64971052d
matt <matt+stealth@mahabal.me>
2016-07-15 12:59:11 -0400

    Add total university count to the dashboard

--- src/main/java/org/mahabal/project/servlet/DashboardServlet.java
+++ src/main/java/org/mahabal/project/servlet/DashboardServlet.java
@@ -52,13 +52,14 @@ public class DashboardServlet extends ProjectServlet {
                     long activeSessionCount = -1;
                     long rsoCount = -1;
                     long unapprovedRsoCount = -1;
+                    long uniCount = -1;
 
                     String total_students = "total_students";
                     String total_rsos = "total_rsos";
                     String unapproved_rsos = "unapproved_rsos";
                     String total_sessions = "total_sessions";
                     String active_sessions = "active_sessions";
-
+                    String total_universities = "total_universities";
 
                     String rso = req.getParameter("r");
 
@@ -70,6 +71,7 @@ public class DashboardServlet extends ProjectServlet {
                             String totalStudentCount = "select count(*) from student";
                             String totalSessionCount = "select count(*) from session";
                             String totalRsosCount = "select count(*) from rso_data";
+                            String totalUniCount = "select count(*) from university";
                             String unapprovedRsosConstraint = "where rso_data.approved = 0";
                             String activeSessionConstraint = "where session.created > now() - interval 24 hour";
 
@@ -78,12 +80,14 @@ public class DashboardServlet extends ProjectServlet {
                                             "(%s) as %s," +
                                             "(%s %s) as %s," +
                                             "(%s) as %s," +
-                                            "(%s %s) as %s;",
+                                            "(%s %s) as %s," +
+                                            "(%s) as %s;",
                                     totalStudentCount, total_students,
                                     totalSessionCount, total_sessions,
                                     totalSessionCount, activeSessionConstraint, active_sessions,
                                     totalRsosCount, total_rsos,
-                                    totalRsosCount, unapprovedRsosConstraint, unapproved_rsos
+                                    totalRsosCount, unapprovedRsosConstraint, unapproved_rsos,
+                                    totalUniCount, total_universities
                             ));
                             if (counts.size() > 0) {
                                 userCount = (Long) counts.get(0).get(total_students);
@@ -91,7 +95,7 @@ public class DashboardServlet extends ProjectServlet {
                                 activeSessionCount = (Long) counts.get(0).get(active_sessions);
                                 rsoCount = (Long) counts.get(0).get(total_rsos);
                                 unapprovedRsoCount = (Long) counts.get(0).get(unapproved_rsos);
-
+                                uniCount = (Long) counts.get(0).get(total_universities);
                             }
                         }
                     }
@@ -102,6 +106,7 @@ public class DashboardServlet extends ProjectServlet {
                     o.add(active_sessions, new JsonPrimitive(activeSessionCount));
                     o.add(total_rsos, new JsonPrimitive(rsoCount));
                     o.add(unapproved_rsos, new JsonPrimitive(unapprovedRsoCount));
+                    o.add(total_universities, new JsonPrimitive(uniCount));
                     resp.setStatus(HttpServletResponse.SC_OK);
                     resp.getWriter().println(o);
                     System.out.println(o);
