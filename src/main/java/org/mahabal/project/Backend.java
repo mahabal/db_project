66cd63ac2096c09358ed5fc52dcf90dcd720677a
Matt <matthew@mahabal.org>
2016-07-18 13:05:49 -0400

    Comment out the code that stalled backend

--- src/main/java/org/mahabal/project/Backend.java
+++ src/main/java/org/mahabal/project/Backend.java
@@ -44,60 +44,60 @@ public class Backend {
         handler.addServletWithMapping(new ServletHolder(new UniversityHandler(dbi)), "/university");
         handler.addServletWithMapping(new ServletHolder(new EventHandler(dbi)), "/events");
 
-        final Scanner s = new Scanner(System.in);
-        String line;
-        while ((line = s.nextLine()) != null) {
-            if (line.equals("exit")) {
-                break;
-            } else if (line.equals("ucf")) {
-
-                final int scope = 0; // 0 = public (anyone can come)
-                final int aid = 1;
-
-                String text = Jsoup.connect("http://events.ucf.edu/upcoming/feed.json").ignoreContentType(true).get().text();
-                final JsonArray array = new JsonParser().parse(text).getAsJsonArray();
-
-                System.out.println("Retrieving UCF events ... ");
-                int count = 0;
-
-                for (final JsonElement e : array) {
-                    if (e.isJsonObject()) {
-                        final JsonObject o = e.getAsJsonObject();
-
-                        String name = o.has("title") ? o.get("title").getAsString() : "";
-                        String desc = o.has("description") ? o.get("description").getAsString() : "";
-                        String location = o.has("location") ? o.get("location").getAsString() : "";
-                        String contant_name = o.has("contact_name") ? o.get("contact_name").getAsString() : "";
-                        String contant_phone = "";
-                        String contant_email = o.has("contact_email") ? o.get("contact_email").getAsString() : "";
-                        if (o.has("tags") && o.get("tags").isJsonArray()) {
-                            final JsonArray tagArray = o.getAsJsonArray("tags");
-                            String[] tags = new String[tagArray.size()];
-                            for (int i = 0; i < tagArray.size(); i++) {
-                                tags[i] = tagArray.get(i).getAsString();
-                            }
-                        }
-
-                        try (Handle h = dbi.open()) {
-
-                            try {
-                                Event.Queries event = h.attach(Event.Queries.class);
-                                count += event.insert(new Event(scope, aid, name, desc,
-                                        new Timestamp(System.currentTimeMillis()), location,
-                                        contant_name, contant_phone, contant_email));
-
-                            } catch (final Exception e2) {
-                                e2.printStackTrace();
-                            }
-                        }
-
-                    }
-                }
-
-                System.out.println("Inserted " + count + " entries into the database.");
-
-            }
-        }
+//        final Scanner s = new Scanner(System.in);
+//        String line;
+//        while ((line = s.nextLine()) != null) {
+//            if (line.equals("exit")) {
+//                break;
+//            } else if (line.equals("ucf")) {
+//
+//                final int scope = 0; // 0 = public (anyone can come)
+//                final int aid = 1;
+//
+//                String text = Jsoup.connect("http://events.ucf.edu/upcoming/feed.json").ignoreContentType(true).get().text();
+//                final JsonArray array = new JsonParser().parse(text).getAsJsonArray();
+//
+//                System.out.println("Retrieving UCF events ... ");
+//                int count = 0;
+//
+//                for (final JsonElement e : array) {
+//                    if (e.isJsonObject()) {
+//                        final JsonObject o = e.getAsJsonObject();
+//
+//                        String name = o.has("title") ? o.get("title").getAsString() : "";
+//                        String desc = o.has("description") ? o.get("description").getAsString() : "";
+//                        String location = o.has("location") ? o.get("location").getAsString() : "";
+//                        String contant_name = o.has("contact_name") ? o.get("contact_name").getAsString() : "";
+//                        String contant_phone = "";
+//                        String contant_email = o.has("contact_email") ? o.get("contact_email").getAsString() : "";
+//                        if (o.has("tags") && o.get("tags").isJsonArray()) {
+//                            final JsonArray tagArray = o.getAsJsonArray("tags");
+//                            String[] tags = new String[tagArray.size()];
+//                            for (int i = 0; i < tagArray.size(); i++) {
+//                                tags[i] = tagArray.get(i).getAsString();
+//                            }
+//                        }
+//
+//                        try (Handle h = dbi.open()) {
+//
+//                            try {
+//                                Event.Queries event = h.attach(Event.Queries.class);
+//                                count += event.insert(new Event(scope, aid, name, desc,
+//                                        new Timestamp(System.currentTimeMillis()), location,
+//                                        contant_name, contant_phone, contant_email));
+//
+//                            } catch (final Exception e2) {
+//                                e2.printStackTrace();
+//                            }
+//                        }
+//
+//                    }
+//                }
+//
+//                System.out.println("Inserted " + count + " entries into the database.");
+//
+//            }
+//        }
 
 
     }
